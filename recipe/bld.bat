2b01493564d31bd5b440a6442ee20f7e91796c75
Connor Martin <connormartin7@gmail.com>
2021-01-14 09:46:31 -0600

    use os releases from github

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,22 +1,3 @@
-rem go one level up
-cd %SRC_DIR%
-cd ..
+move git-lfs.exe "%LIBRARY_BIN%\git-lfs.exe"
 
-rem create the gopath directory structure
-set "GOPATH=%CD%\gopath"
-mkdir "%GOPATH%\src\github.com\git-lfs\git-lfs"
-xcopy /s "%SRC_DIR%\*" "%GOPATH%\src\github.com\git-lfs\git-lfs\"
-cd "%GOPATH%\src\github.com\git-lfs\git-lfs"
-
-rem generate the help text, see:
-rem https://github.com/git-lfs/git-lfs/issues/3159
-rem https://github.com/git-lfs/git-lfs/issues/3160
-go generate github.com\git-lfs\git-lfs\commands
-
-rem build the project
-go build -v
-if errorlevel 1 exit 1
-
-rem install the binary
-mkdir "%LIBRARY_BIN%"
-mv "git-lfs" "%LIBRARY_BIN%\git-lfs"
+IF %ERRORLEVEL% NEQ 0 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,23 +1,5 @@
 #!/usr/bin/env bash
 
-# go one level up
-cd $SRC_DIR
-cd ..
+set -o xtrace -o errexit
 
-# create the gopath directory structure
-export GOPATH=$PWD/gopath
-mkdir -p $GOPATH/src/github.com/git-lfs/git-lfs
-cp -rv $SRC_DIR/* $GOPATH/src/github.com/git-lfs/git-lfs
-cd $GOPATH/src/github.com/git-lfs/git-lfs
-
-# generate the help text, see:
-# https://github.com/git-lfs/git-lfs/issues/3159
-# https://github.com/git-lfs/git-lfs/issues/3160
-go generate github.com/git-lfs/git-lfs/commands
-
-# build the project
-go build -v
-
-# install the binary
-mkdir -p $PREFIX/bin
-cp git-lfs $PREFIX/bin
+bash -x install.sh
